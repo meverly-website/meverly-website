7045cda31420267a61a9f4a891b010f51186924d
meverly-website <m.everlybooks@gmail.com>
2026-08-16 09:08:23 +0200

    Ajout sitemap et robots

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,30 +1,11 @@
 import type { MetadataRoute } from "next";
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: "https://meverly.fr",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1,
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
     },
-    {
-      url: "https://meverly.fr/before-i-knew-you",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.9,
-    },
-    {
-      url: "https://meverly.fr/contact",
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.5,
-    },
-    {
-      url: "https://meverly.fr/mentions-legales",
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.3,
-    },
-  ];
+    sitemap: "https://meverly.fr/sitemap.xml",
+  };
 }
